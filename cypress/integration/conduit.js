df96d17833a4595ffa7419db317db4746691dc90
zhwu <zhuoli.wu@outlook.com>
2020-04-17 17:02:33 +0200

    creation cypress

---
+++ cypress/integration/conduit.js
@@ -0,0 +1,14 @@
+describe('the conduit application', () => {
+  it('shows some posts', () => {
+    cy.visit('/')
+    cy.get('.article-preview').should('have.length',10)
+    })
+   it('shows the first posts', () => {
+    cy.server()
+    // we set the response to be the activites.json fixture
+    cy.route('GET', 'api/articles*', 'fixture:posts.json')
+    cy.visit('/')
+    cy.get(':nth-child(1) > .article-preview').contains('test-moh')
+    })
+  })
+})
