59c8759fd7bd2ae8b0587ca548a90088e80d6b76
zhwu <zhuoli.wu@outlook.com>
2020-04-17 18:49:24 +0200

    commit conduit

--- cypress/integration/conduit.js
+++ cypress/integration/conduit.js
@@ -1,14 +1,19 @@
 describe('the conduit application', () => {
   it('shows some posts', () => {
-    cy.visit('/')
-    cy.get('.article-preview').should('have.length',10)
+        cy.visit('/')
+        cy.get('.article-preview').should('have.length',10)
     })
    it('shows the first posts', () => {
-    cy.server()
-    // we set the response to be the activites.json fixture
-    cy.route('GET', 'api/articles*', 'fixture:posts.json')
-    cy.visit('/')
-    cy.get(':nth-child(1) > .article-preview').contains('test-moh')
+        cy.server()
+        // we set the response to be the activites.json fixture
+        cy.route('GET', 'api/articles*', 'fixture:posts.json')
+        cy.visit('/')
+        cy.get(':nth-child(1) > .article-preview').contains('test-moh')
     })
+    it('should handle an empty database', () => {
+        cy.server()
+        cy.route('GET', '/api/articles*', 'fixture:no_posts.json')
+        cy.visit('/')
+        cy.contains('No articles are here... yet.')
   })
 })
